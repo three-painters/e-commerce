d875689e3d72c493dc9a51c72ca95761b20ef4c6
刘冬 <liudong@liudongdeMacBook-Pro.local>
2020-03-11 21:45:32 +0800

    Initial commit

---
+++ core-management/core-management-server/src/main/java/com/lxy/CoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.lxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreManagementApplication.class, args);
+	}
+
+}
