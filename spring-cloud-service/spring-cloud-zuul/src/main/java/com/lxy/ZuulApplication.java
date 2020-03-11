fe9885d0882d178463483d1f502aab1e05f8d4ee
liudong <ld19950104@163.com>
2020-03-11 23:11:53 +0800

    初始提交

--- spring-cloud-service/spring-cloud-zuul/src/main/java/com/lxy/ZuulApplication.java
+++ spring-cloud-service/spring-cloud-zuul/src/main/java/com/lxy/ZuulApplication.java
@@ -2,8 +2,14 @@ package com.lxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ZuulApplication {
 
     public static void main(String[] args) {
