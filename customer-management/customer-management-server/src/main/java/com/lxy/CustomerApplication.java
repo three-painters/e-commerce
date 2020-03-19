04a98e530354dfb898f30c30e7c0fff2f801dfaa
Donily <ld19950104@163.com>
2020-03-19 11:32:27 +0800

    初始提交

---
+++ customer-management/customer-management-server/src/main/java/com/lxy/CustomerApplication.java
@@ -0,0 +1,13 @@
+package com.lxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerManagementServerApplication.class, args);
+    }
+
+}
---
+++ customer-management/customer-management-server/src/test/java/com/lxy/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxy;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
