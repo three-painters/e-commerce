67feab2083f7d725467c58fa368630c9e31747e0
Donily <ld19950104@163.com>
2020-03-19 13:48:57 +0800

    初始提交

--- customer-management/customer-management-server/src/main/java/com/lxy/CustomerApplication.java
+++ customer-management/customer-management-server/src/main/java/com/lxy/CustomerApplication.java
@@ -1,13 +1,19 @@
 package com.lxy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class CustomerManagementServerApplication {
+@EnableFeignClients
+@EnableDiscoveryClient
+@MapperScan("com.lxy.mapper")
+public class CustomerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CustomerManagementServerApplication.class, args);
+        SpringApplication.run(CustomerApplication.class, args);
     }
 
 }
