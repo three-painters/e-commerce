b8a03943943cc7d0da02833075d26126f6a41afd
Donily <ld19950104@163.com>
2020-04-25 16:42:55 +0800

    zuul集成oauth2 自定义转发token

--- product-management/product-management-server/src/main/java/com/lxy/config/ResouceServerConfig.java
+++ product-management/product-management-server/src/main/java/com/lxy/config/ResouceServerConfig.java
@@ -33,7 +33,7 @@ public class ResouceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**").access("#oauth2.hasScope('all')")
+                .antMatchers("/**").access("#oauth2.hasScope('ROLE_API')")
                 .and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
